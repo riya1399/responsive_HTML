fe137c5de9f83ff3a6fd910d15c3d44a69032281
riya1399 <78474602+riya1399@users.noreply.github.com>
2021-02-23 17:06:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function openMenu() { 
+    var v = document.getElementById("sidemenu"); 
+    v.classList.add("open"); 
+}
+function closeMenu() { 
+    var v = document.getElementById("sidemenu"); 
+    v.classList.remove("open"); 
+}
